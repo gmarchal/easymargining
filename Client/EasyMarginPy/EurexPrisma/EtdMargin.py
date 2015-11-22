306de9d530baf476e3da43b235a02a5b07f695e0
mlerebours <modo.cl201_github@m4am.net>
2015-11-22 19:29:12 +0100

    Sample REST requests

--- Client/EasyMarginPy/EurexPrisma/EtdMargin.py
+++ Client/EasyMarginPy/EurexPrisma/EtdMargin.py
@@ -1,4 +1,6 @@
 import requests
+import pandas as pd
+from pandas.io import json
 
 URL_SERVER = 'http://localhost:8080'
 HELLO_PATH = '/'
@@ -14,5 +16,29 @@ def etd_margin():
     r = requests.get(URL_SERVER + PRISMA_EUREX_PATH + '/etdMargin')
     return r.text
 
+
+def portfolio():
+    r = requests.get(URL_SERVER + PRISMA_EUREX_PATH + '/portfolio')
+    return r.json
+
+
+def get_trade():
+    r = requests.get(URL_SERVER + PRISMA_EUREX_PATH + '/trade')
+    return r.json()
+
+
+def add_trade(trade):
+    r = requests.post(URL_SERVER + PRISMA_EUREX_PATH + '/trade', json=trade)
+    return r.text
+
+
+print(hello())
+print(get_trade())
+
+trade = {'assignedNotifiedBalance': None, 'exerciseStyleFlag': None, 'expiryDate': None, 'longBalance': None,
+         'productSettlementType': None, 'shortBalance': None, 'exercisedAllocatedBalance': None, 'instrumentType': None,
+         'productId': 'ORDX', 'versionNumber': None, 'exercisePrice': None, 'callPutFlag': None}
+
+print(add_trade(trade))
 
 print(etd_margin())
