ed1bc4f2afd701ab86f4be61675616dcf47d28ca
mlerebou <mederic.lerebours@sgcib.com>
2015-11-24 13:04:34 +0100

    Read/Write TradeFile

--- Server/src/main/java/com/socgen/finit/easymargin/converter/TradeFileHandler.java
+++ Server/src/main/java/com/socgen/finit/easymargin/converter/TradeFileHandler.java
@@ -4,6 +4,12 @@ import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.bean.CsvToBean;
 import au.com.bytecode.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
 import com.socgen.finit.easymargin.model.TradeEntity;
+import com.univocity.parsers.common.processor.BeanListProcessor;
+import com.univocity.parsers.common.processor.BeanWriterProcessor;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
+import com.univocity.parsers.csv.CsvWriter;
+import com.univocity.parsers.csv.CsvWriterSettings;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -11,6 +17,7 @@ import org.testng.collections.Maps;
 
 import javax.annotation.PostConstruct;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -25,37 +32,40 @@ import java.util.stream.Collectors;
 @Slf4j
 public class TradeFileHandler {
 
-    Map<String, String> columnMapping;
-
-    @PostConstruct
-    public void init() {
-        log.info("INIT TradeFileHandler");
-        columnMapping = Maps.newHashMap();
-        columnMapping.put("Product ID", "ProductId");
-        columnMapping.put("Expiry Year", "expiryYear");
-        columnMapping.put("Expiry Month", "expiryMonth");
-        columnMapping.put("Expiry Day", "expiryDay");
-        columnMapping.put("Version Number", "versionNumber");
-        columnMapping.put("Product Settlement Type", "productSettlementType");
-        columnMapping.put("Call Put Flag", "callPutFlag");
-        columnMapping.put("Exercise Price", "exercisePrice");
-        columnMapping.put("Exercise Style Flag", "exerciseStyleFlag");
-        columnMapping.put("Instrument Type", "instrumentType");
-        columnMapping.put("Assigned/Notified Balance", "assignedNotifiedBalance");
-        columnMapping.put("Exercised/Allocated Balance", "exercisedAllocatedBalance");
-        columnMapping.put("Long Balance", "longBalance");
-        columnMapping.put("Short Balance", "shortBalance");
-
-    }
-
     public List<TradeEntity> readTradeFile(URL file) throws IOException {
-        HeaderColumnNameTranslateMappingStrategy<TradeEntity> strat = new HeaderColumnNameTranslateMappingStrategy<TradeEntity>();
-        strat.setType(TradeEntity.class);
-        strat.setColumnMapping(columnMapping);
+        log.info("Read trade file " + file);
+        // BeanListProcessor converts each parsed row to an instance of a given class, then stores each instance into a list.
+        BeanListProcessor<TradeEntity> rowProcessor = new BeanListProcessor<TradeEntity>(TradeEntity.class);
 
-        CsvToBean csv = new CsvToBean();
-        CSVReader reader = new CSVReader(new FileReader(file.getFile()), ',', '"', 0);
-        List<TradeEntity> parse = csv.parse(strat, reader);
-        return parse.stream().filter(t -> !StringUtils.isEmpty(t.getProductId())).collect(Collectors.toList());
+        CsvParserSettings parserSettings = new CsvParserSettings();
+        parserSettings.setRowProcessor(rowProcessor);
+        parserSettings.setHeaderExtractionEnabled(true);
+
+        CsvParser parser = new CsvParser(parserSettings);
+        parser.parse(new FileReader(file.getFile()));
+
+        // The BeanListProcessor provides a list of objects extracted from the input.
+        List<TradeEntity> beans = rowProcessor.getBeans();
+        log.info("Trade file " + file + ": " + beans.size() + " trades.");
+        return beans;
     }
+
+    public void writeTradeFile(List<TradeEntity> tradeEntities, URL file) throws IOException {
+        log.info("Write " + tradeEntities.size() +  " trades in file " + file);
+        // BeanListProcessor converts each parsed row to an instance of a given class, then stores each instance into a list.
+        CsvWriterSettings csvWriterSettings = new CsvWriterSettings();
+        csvWriterSettings.setRowWriterProcessor(new BeanWriterProcessor<>(TradeEntity.class));
+        csvWriterSettings.setHeaders("Product ID", "Expiry Year", "Expiry Month", "Expiry Day", "Version Number", "Product Settlement Type", "Call Put Flag", "Exercise Price", "Exercise Style Flag", "Instrument Type", "Assigned/Notified Balance", "Exercised/Allocated Balance", "Long Balance", "Short Balance");
+        CsvWriter writer = new CsvWriter(new FileWriter(file.getFile()), csvWriterSettings);
+
+        // Write the record headers of this file
+        writer.writeHeaders();
+
+        // Here we just tell the writer to write everything and close the given output Writer instance.
+        writer.processRecords(tradeEntities);
+
+        writer.close();
+        log.info("Done.");
+    }
+
 }
--- Server/src/test/java/com/socgen/finit/easymargin/converter/TradeFileHandlerTest.java
+++ Server/src/test/java/com/socgen/finit/easymargin/converter/TradeFileHandlerTest.java
@@ -1,5 +1,6 @@
 package com.socgen.finit.easymargin.converter;
 
+import com.beust.jcommander.internal.Lists;
 import com.opengamma.margining.eurex.prisma.data.FileResources;
 import com.socgen.finit.easymargin.Application;
 import com.socgen.finit.easymargin.DefaultConfig;
@@ -20,6 +21,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.net.URL;
 import java.util.List;
 
@@ -38,7 +40,7 @@ public class TradeFileHandlerTest {
     private TradeFileHandler tradeFileHandler;
 
     @Test
-    public void testReadTradeFile() throws Exception {
+    public void testReadWriteTradeFile() throws Exception {
         Assert.notNull(tradeFileHandler);
 
         URL url = FileResources.byPath("trade/etdTrades.csv");
@@ -47,6 +49,32 @@ public class TradeFileHandlerTest {
 
         List<TradeEntity> tradeEntities = tradeFileHandler.readTradeFile(url);
         Assert.notEmpty(tradeEntities);
+
+        //create a temp file
+        File temp = File.createTempFile("temp-etdTrades", ".csv");
+
+        tradeFileHandler.writeTradeFile(tradeEntities, temp.toURL());
+        List<TradeEntity> readTradeEntities = tradeFileHandler.readTradeFile(temp.toURL());
+        Assert.notEmpty(readTradeEntities);
+        Assert.isTrue(readTradeEntities.size() == tradeEntities.size());
+        for (TradeEntity tradeEntity : tradeEntities) {
+            Assert.isTrue(readTradeEntities.contains(tradeEntity));
+        }
+    }
+
+    @Test
+    public void testWriteTradeFile() throws Exception {
+        Assert.notNull(tradeFileHandler);
+
+        URL url = FileResources.byPath("trade/etdTrades.csv");
+
+        Assert.notNull(url);
+
+        List<TradeEntity> tradeEntityList = Lists.newArrayList();
+        TradeEntity trade = new TradeEntity();
+        trade.setProductId("ORDX");
+        tradeEntityList.add(trade);
+        tradeFileHandler.writeTradeFile(tradeEntityList, url);
         System.out.println("Done");
     }
 }
