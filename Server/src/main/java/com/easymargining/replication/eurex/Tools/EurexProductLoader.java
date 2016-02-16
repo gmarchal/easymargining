e6f8b4b9c0faa5069b086cd764d7d5b353289140
gmarchal <gilles.marchal@sgcib.com>
2016-02-16 18:09:57 +0100

    Review Tools, to manage Eurex STL files

--- Server/src/main/java/com/easymargining/replication/eurex/Tools/EurexProductLoader.java
+++ Server/src/main/java/com/easymargining/replication/eurex/Tools/EurexProductLoader.java
@@ -1,11 +1,16 @@
 package com.easymargining.replication.eurex.Tools;
 
+import com.opengamma.margining.eurex.prisma.data.MarketDataFileResolver;
+import com.opengamma.margining.eurex.prisma.replication.market.parsers.EurexProductDefinition;
+import com.opengamma.margining.eurex.prisma.replication.market.parsers.EurexScenarioPricesParser;
 import com.opengamma.margining.eurex.prisma.replication.market.parsers.EurexSettlementPriceDefinition;
 import com.opengamma.margining.eurex.prisma.replication.market.parsers.EurexSettlementPricesParser;
 import lombok.extern.slf4j.Slf4j;
+import org.threeten.bp.LocalDate;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -17,18 +22,56 @@ public class EurexProductLoader {
 
     public static void main(String[] args) {
 
+
         EurexSettlementPricesParser parser = new EurexSettlementPricesParser();
 
+
+
+
         try {
-            log.info("Load Settlement Price definition ...");
+            log.info("Load Eurex product definition ...");
+
+            LocalDate s_valuationDate = LocalDate.of(2015, 6, 3);
+            // Use file resolver utility to discover data from standard Eurex directory structure
+            MarketDataFileResolver fileResolver = new MarketDataFileResolver("marketData", s_valuationDate);
 
             // Small File "Server/src/main/resources/marketData/20150603//ETD/00stlpricepubli20150603eodx.zip"
+            //
+            List<EurexProductDefinition> productDefinitions =
+                    EurexScenarioPricesParser.parse(
+                            new File("D:/gmarchal/eurex-data/marketdata/20151127/ETD/00THEOINSTPUBLI20151127NISERIESEODX0001_0005.TXT.ZIP").toURI().toURL());
+
             // Big File "D:/gmarchal/eurex-data/marketdata/20151127/ETD/00THEOINSTPUBLI20151127NISERIESEODX0001_0005.TXT.ZIP"
-            List<EurexSettlementPriceDefinition> definitions =
-                    parser.parse(new File("D:/gmarchal/eurex-data/marketdata/20151127/ETD/00THEOINSTPUBLI20151127NISERIESEODX0001_0005.TXT.ZIP").toURI().toURL());
+            //List<EurexSettlementPriceDefinition> definitions =
+            //        parser.parse(new File("D:/gmarchal/eurex-data/marketdata/20151127/ETD/00THEOINSTPUBLI20151127NISERIESEODX0001_0005.TXT.ZIP").toURI().toURL());
 
-            log.info("End of Loading Settlement Price definition ...");
+            log.info("End of Loading Eurex Product definition : " + productDefinitions.size() + "products");
 
+            productDefinitions.forEach(
+                    (eurexProductDefinition) -> {
+                        log.info(" --- " +
+                                eurexProductDefinition.getProductId()+                              // Product Id
+                                " - " +
+                                eurexProductDefinition.getContractYear() +                          // Expiry Year
+                                " - " +
+                                eurexProductDefinition.getContractMonth() +                         // Expiry Month
+                                " - " +
+                                eurexProductDefinition.getSeriesDefinition().getSettlementType() +  // Settlement Type
+                                " - " +
+                                eurexProductDefinition.getSeriesDefinition().getCallPutFlag() +     // Call Put Flag
+                                " - " +
+                                eurexProductDefinition.getSeriesDefinition().getExercisePrice() +   // Exercice Price
+                                " - " +
+                                eurexProductDefinition.getSeriesDefinition().getExerciseFlag() +    // Exercice Style Flag
+                                " - " +
+                                eurexProductDefinition.getSeriesDefinition().getExerciseFlag() +    // Exercice Style Flag
+                                " - " +
+                                eurexProductDefinition.getSeriesDefinition().getExerciseFlag()      // Exercice Style Flag
+                        );
+                    }
+            );
+
+            /*
             definitions.forEach(
                     (eurexSettlementPriceDefinition) -> {
                         log.info(" --- " +
@@ -44,8 +87,9 @@ public class EurexProductLoader {
                         );
                     }
             );
+            */
 
-            log.info("End of printing : Settlement Price definition");
+            log.info("End of printing : Eurex Product definition: " + productDefinitions.size() + " products");
 
         } catch (IOException e) {
             e.printStackTrace();
